8ead421f08160dc2d6a6a7767aec9f112bcf5978
Michael Gruener <michael.gruener@chaosmoon.net>
2014-10-28 22:57:18 +0100

    Initial commit

---
+++ puppet-checks.groovy
@@ -0,0 +1,9 @@
+/**
+ * Gitblit Pre-Receive Hook: puppet-checks
+ *
+ * Perform the following checks:
+ *  - puppet syntax checks
+ *  - puppet-lint style checks
+ *  - erb template syntax checks
+ *  - yaml (hiera data) syntax checks
+ */
